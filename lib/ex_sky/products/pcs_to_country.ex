a26f2d8a15af7baaac031bcd398bc1f26c7ba399
Artur Uklejewski <artur.uklejewski@gmail.com>
2021-10-06 22:24:42 +0200

    Remove bad validation

--- lib/ex_sky/products/pcs_to_country.ex
+++ lib/ex_sky/products/pcs_to_country.ex
@@ -17,21 +17,7 @@ defmodule ExSky.Products.PcsToCountry do
   def changeset(pcs_to_countries, attrs) do
     pcs_to_countries
     |> cast(attrs, [:temp_id, :checked?, :country, :number_of_products, :number_of_copies])
-    |> validate_number(:number_of_products, greater_than: 1)
+    |> validate_number(:number_of_products, greater_than: 0)
     |> validate_required([:country])
-    |> validate_checked?()
-  end
-
-  # TODO: Validation is not working
-  def validate_checked?(changeset) do
-    case get_field(changeset, :checked?) do
-      true ->
-        get_field(changeset, :number_of_products)
-
-        validate_number(changeset, :number_of_products, greater_than: 1)
-
-      false ->
-        changeset
-    end
   end
 end
